64e78675a18d31f67f338bc8804536eabffe9f25
colefortner <colefortner@gmail.com>
2024-03-14 14:09:53 -0500

    Initial Commit

---
+++ healthapptest/healthapptestApp.swift
@@ -0,0 +1,17 @@
+//
+//  healthapptestApp.swift
+//  healthapptest
+//
+//  Created by Cole Fortner on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct healthapptestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
